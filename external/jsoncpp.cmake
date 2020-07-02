f9a2fda9d8a8f43ab9cb0d1a146d4961f24a459c
flagarde <lagarde@sjtu.edu.cn>
2020-07-02 15:59:58 +0800

    Better jsoncpp

--- external/jsoncpp.cmake
+++ external/jsoncpp.cmake
@@ -1,8 +1,21 @@
+if(USE_SYSTEM_JSONCPP)
+  find_package(jsoncpp QUIET)
+  if(jsoncpp_FOUND)
+    set(COMPILE_JSONCPP FALSE)
+  else()
+    set(COMPILE_JSONCPP TRUE)
+    message(STATUS "jsoncpp not found ! jsoncpp will be compiled !") 
+  endif()
+else()
+  set(COMPILE_JSONCPP TRUE)
+  message(STATUS "jsoncpp will be compiled") 
+endif()
+
+if(COMPILE_JSONCPP)
 if (NOT TARGET jsoncpp_project)
 include(ExternalProject)
 # ----- jsoncpp_project package -----
-  ExternalProject_Add(
-                      jsoncpp_project
+  ExternalProject_Add(jsoncpp_project
                       GIT_REPOSITORY ${JSONCPP_REPOSITORY}
                       GIT_TAG ${JSONCPP_VERSION}
                       GIT_PROGRESS TRUE
@@ -17,5 +30,12 @@ include(ExternalProject)
   add_dependencies(jsoncpp_internal jsoncpp_project)
   target_link_libraries(jsoncpp_internal INTERFACE jsoncpp)
   target_include_directories(jsoncpp_internal INTERFACE "${INCLUDE_OUTPUT_DIR}/json")
+  add_library(jsoncpp::jsoncpp ALIAS jsoncpp_internal)
+endif()
+else()
+  add_library(jsoncpp_internal INTERFACE)
+  target_link_libraries(jsoncpp_internal INTERFACE  jsoncpp_lib)
+  get_target_property(JSON_INC_PATH jsoncpp_lib INTERFACE_INCLUDE_DIRECTORIES)
+  target_include_directories(jsoncpp_internal INTERFACE "${JSON_INC_PATH}/json")
   add_library(jsoncpp::jsoncpp ALIAS jsoncpp_internal)
 endif()
